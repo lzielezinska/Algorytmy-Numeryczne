4825b131cb19100d66308be34219c26d0266ce80
Maniakrzelaza <maniakrzelaza@gmail.com>
2018-11-09 12:54:57 +0100

    kd-przed-pulem

--- projekt2/src/Main.java
+++ projekt2/src/Main.java
@@ -4,19 +4,15 @@ public class Main {
     public static void main(String[] args){
         //Debug section
         System.out.println("hello");
-        ANumber debugNum1 = new MyType(new BigInteger("2"), new BigInteger("3"));
-        ANumber debugNum2 = new MyType(4, 5);
-        ANumber debugAdd = (ANumber)debugNum1.add(debugNum2);
-        ANumber debugSub = (ANumber)debugNum1.sub(debugNum2);
-        ANumber debugMul = (ANumber)debugNum1.mul(debugNum2);
-        ANumber debugDiv = (ANumber)debugNum1.div(debugNum2);
+        MyType debugNum1 = new MyType(new BigInteger("2"), new BigInteger("3"));
+        MyType debugNum2 = new MyType(new BigInteger("4"), new BigInteger("5"));
+        MyType debugAdd = debugNum1.add(debugNum2);
+        MyType debugSub = debugNum1.sub(debugNum2);
+        MyType debugMul = debugNum1.mul(debugNum2);
+        MyType debugDiv = debugNum1.div(debugNum2);
         System.out.println(debugAdd.toString());
         System.out.println(debugSub.toString());
         System.out.println(debugMul.toString());
         System.out.println(debugDiv.toString());
-        for (int i = 0; i < 1000; i++){
-            System.out.println(MyType.generateRandomNumber());
-        }
-
     }
 }
